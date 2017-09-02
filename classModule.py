00de6e34a33a53b1ff646a7ccdda431c9f5d062c
lmm <damnit1989@163.com>
2017-09-02 11:57:29 +0800

    类的访问控制

--- classModule.py
+++ classModule.py
@@ -25,6 +25,10 @@ a.getName()
 print(a.all)
 
 #双下划线开头的实例变量是不是一定不能从外部访问呢？其实也不是。不能直接访问__name是因为Python解释器对外把__name变量改成了_TestImport__name，所以，仍然可以通过_TestImport__name来访问__name私有变量：
+
+#在一个实例里， __girl表示“我是贞女，你不能上我”
+# _girl表示“你虽然可以上我，但你应该把我看做贞女” 
+#girl表示“我是荡妇，谁都可以上我” 但是python仍然可以用_类名__girl强上贞女
 print(a._TestImport__name)
 
 
