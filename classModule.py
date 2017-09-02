4627fabf22f85694bd3d90361a464b3409b7e945
lmm <damnit1989@163.com>
2017-09-02 11:55:50 +0800

    类的访问限制

--- classModule.py
+++ classModule.py
@@ -2,18 +2,32 @@
 
 ' a class module '
 
-# from testclass import TestImport
+__author__ = 'lmm'
 
+
+
+#第一种导入模块方法
+from testclass import TestImport
+a = TestImport()
+a.getName()
+
+
+#第二种导入模块方法
 import testclass
 a = testclass.TestImport().getName
 a()
 
 
+#第三种导入模块方法
 import testclass
-a = testclass.TestImport()
+a = testclass.TestImport('liudao',30)
 a.getName()
+print(a.all)
+
+#双下划线开头的实例变量是不是一定不能从外部访问呢？其实也不是。不能直接访问__name是因为Python解释器对外把__name变量改成了_TestImport__name，所以，仍然可以通过_TestImport__name来访问__name私有变量：
+print(a._TestImport__name)
+
 
 
 if __name__ == '__main__':
 	pass
-	print('good')
--- testclass.py
+++ testclass.py
@@ -1,14 +1,25 @@
 # -*- coding: utf-8 -*-
+#python的类和实例
 
 ' a class module '
 
-class TestImport:
-	# self_name = 'lmm'
-	# self_age = 23
+__author__ = 'lmm'
+
+
+#如果要让内部属性不被外部访问，可以把属性的名称前加上两个下划线__，在Python中，实例的变量名如果以__开头，就变成了一个私有变量（private），只有内部可以访问，外部不能访问
+
+#在一个实例里， __girl表示“我是贞女，你不能上我”
+# _girl表示“你虽然可以上我，但你应该把我看做贞女” 
+#girl表示“我是荡妇，谁都可以上我” 但是python仍然可以用_实例名__girl强上贞女
+class TestImport(object):
+
+	def __init__(self,name='lmm',age = 20):
+		self.__name = name
+		self.__age = age
+		self.all = [name,age]
 	def getName(self):
-		print('sss')
+		print("your name is %s" % (self.__name))
 	def getAge(self):
-		print('dddd')
-	
+		print("your age is %d" % (self.__age))
 # a = TestImport()
 
