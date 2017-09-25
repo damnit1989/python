c96a5948adf49aa64fa2fdc0b21ef2507720736c
lmm <damnit1989@163.com>
2017-09-25 11:17:26 +0800

    ConfigParser 模块 读取配置文件

--- read_conf.py
+++ read_conf.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 #读取配置文件conf.cgf
 
-'read conf example'
+'''
+read conf example,you can use the __doc__  print out the infomation
+please just do  it
+
+'''
 
 __author__ = 'lmm'
 
 from ConfigParser import ConfigParser
+import time 
 
 conf_file = 'conf.cfg'
 conf = ConfigParser()
@@ -13,4 +18,7 @@ conf.read(conf_file)
 
 print conf.get('info','name'),conf.get('info','age')
 
+time .sleep(3)
+
 print conf.get('addr','born')
+print conf.get('addr','city')
