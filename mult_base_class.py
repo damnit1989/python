7a4e2ad6104a24068e98ea99a1be69f5fc611535
Your Name <damnit1989@163.com>
2017-09-27 08:44:45 -0700

    mult base class module

--- mult_base_class.py
+++ mult_base_class.py
@@ -8,17 +8,23 @@ __author__ = 'lmm'
 
 
 class Calculator:
+    value = 0
     def calculate(self,expression):
         self.value = eval(expression)
 
 class Talker:
+    value = 1
     def talk(self):
         print 'hi my value is ',self.value
 
-class TalkingCalculator(Calculator,Talker):
+#
+#class TalkingCalculator(Calculator,Talker):
+class TalkingCalculator(Talker,Calculator):
     pass
 
 
 tc = TalkingCalculator()
+print(tc.value)
 tc.calculate('1+2*3')
 tc.talk()
+print(tc.value)
