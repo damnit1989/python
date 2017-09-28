0fe18f9c1b9cf2946035cb213fe391d1575cf3ea
lmm <damnit1989@163.com>
2017-09-28 21:52:12 +0800

    django
    django

--- HelloWorld/HelloWorld/urls.py
+++ HelloWorld/HelloWorld/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 
-from . import view,search,testdb
+from TestModel import view,search,testdb
 # import testdb
 
 urlpatterns = [
