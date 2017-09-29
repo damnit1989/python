438187774519f575d00b45f6fef085b917dc6903
lmm <damnit1989@163.com>
2017-09-29 14:56:16 +0800

    django
    django

--- HelloWorld/TestModel/urls.py
+++ HelloWorld/TestModel/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^list/$',view.hello,name="list"),
     url(r'^(?P<id>[0-9]+)/$',view.detail,name="detail"),
     url(r'^form/$',view.get_name,name="form"),
+    url(r'^get_name/$',view.get_name,name="form"),
     # url(r'^hello/',view.hello),
     # url(r'^search_form/',search.search_form),
     # url(r'^search_get',search.search_get),
