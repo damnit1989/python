dc17f7de1472c8859c6aecf110f17cfd84fc3c65
lmm <damnit1989@163.com>
2017-09-29 12:08:58 +0800

    django
    django

--- HelloWorld/TestModel/admin.py
+++ HelloWorld/TestModel/admin.py
@@ -3,7 +3,11 @@ from TestModel.models import Test
 from TestModel.models import Album
 from TestModel.models import article
 
+
+class articleAdmin(admin.ModelAdmin):
+    list_display = ('title','create_date','publish_date')
 # Register your models here.
 admin.site.register(Test)
 admin.site.register(Album)
-admin.site.register(article)
+# admin.site.register(article)
+admin.site.register(article,articleAdmin)
--- HelloWorld/TestModel/models.py
+++ HelloWorld/TestModel/models.py
@@ -9,6 +9,9 @@ from django.utils import timezone
 class Test(models.Model):
     name = models.CharField(max_length=20)
     
+    def __str__(self):
+        return self.name
+    
 class Album(models.Model):
     pass
 
