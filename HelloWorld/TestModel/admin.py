485207b6504d7470157949da4c6428b29fcd8e63
lmm <damnit1989@163.com>
2017-09-29 12:29:18 +0800

    django
    django

--- HelloWorld/TestModel/admin.py
+++ HelloWorld/TestModel/admin.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.contrib import admin
 from TestModel.models import Test
 from TestModel.models import Album
@@ -5,7 +7,10 @@ from TestModel.models import article
 
 
 class articleAdmin(admin.ModelAdmin):
+    #让对象显示字段
     list_display = ('title','create_date','publish_date')
+   
+   
 # Register your models here.
 admin.site.register(Test)
 admin.site.register(Album)
