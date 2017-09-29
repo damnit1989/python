b519b3dbfdefc839ef04de4d9c5822949be1ed85
lmm <damnit1989@163.com>
2017-09-29 20:53:05 +0800

    django
    django

--- HelloWorld/TestModel/forms.py
+++ HelloWorld/TestModel/forms.py
@@ -5,4 +5,4 @@ from django import forms
 class NameForm(forms.Form):
     your_name = forms.CharField(label='Your name', max_length=100)
     your_pswd = forms.CharField(label='Your pswd', max_length=100)
-    
+
