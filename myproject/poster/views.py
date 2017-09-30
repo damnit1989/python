8472087264afaf1d1f48497a036a120eb992017b
lmm <damnit1989@163.com>
2017-09-30 13:24:28 +0800

    Aggregate
    Aggregate

--- myproject/poster/views.py
+++ myproject/poster/views.py
@@ -48,4 +48,4 @@ def send_review_email():
 
 def thankyou(request):
     tweets_in_queue = Tweet.objects.filter(state = 'pending').aggregate(count = Count(id)).values()[0]
-    return render(request,'thankyou.html',{'tweets_in_queue':tweets_in_queue})
+    return render(request,'thank_you.html',{'tweets_in_queue':tweets_in_queue})
