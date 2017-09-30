fd91946051f2dbcc7adf2b783437d9b0d9e3cffc
lmm <damnit1989@163.com>
2017-09-30 14:22:08 +0800

    class = "{%cycle 'oddrow' 'evenrow'%}"
     class = "{%cycle 'oddrow' 'evenrow'%}"

--- myproject/approver/urls.py
+++ myproject/approver/urls.py
@@ -7,5 +7,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.list_tweets,name="list_tweets"),
-    # url(r'^review/(?P<tweet_id>[0-9]+)/$',views.review_tweet,name="review"),
+    url(r'^review/(?P<tweet_id>[0-9]+)/$',views.review_tweet,name="review"),
 ]
