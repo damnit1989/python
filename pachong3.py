0067d2accd3122042c12e92df94fa09498a8d984
Your Name <damnit1989@163.com>
2017-10-19 08:53:09 -0700

    bitch shit

--- pachong3.py
+++ pachong3.py
@@ -2,6 +2,13 @@
 # -*- coding: utf-8 -*-
 # bitch，shit
 
+'''
+有些tag属性在搜索不能使用,比如HTML5中的 data-* 属性
+可以通过 find_all() 方法的 attrs 参数定义一个字典参数来搜索包含特殊属性的tag
+data_soup.find_all(attrs={"data-foo": "value"})
+[<div data-foo="value">foo!</div>]
+'''
+
 
 from bs4 import BeautifulSoup
 import urllib, urllib2
@@ -9,8 +16,15 @@ from lxml import etree
 import os
 
 
-def request_url(url):
+def init_headers():
+    user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
     headers = {'User-Agent':user_agent,}
+    return headers
+
+
+
+def request_url(url,headers):
+
     request = urllib2.Request(url,None,headers)
     u_handle = urllib2.urlopen(request)
     html_str = u_handle.read()
@@ -22,10 +36,9 @@ def get_content(p):
     for video in videos:
         print url+video['src']
         get_img(url+video['src'])  
-    print '第 '+str(p)+' 页抓取成功\n\n'
+    print '第 '+str(p)+' 页抓取完成\n\n'
 
     
-
 def get_img(url):
     apath = os.path.abspath('.')
     img_dir = apath+'/bitch'
@@ -33,8 +46,8 @@ def get_img(url):
     # 判断目录是否存在,不存在则创建
     if not os.path.isdir(img_dir):
         os.mkdir(img_dir)
-    
-    data = request_url(url)
+
+    data = request_url(url,headers)
     
     # 截取url末尾的图片名称
     img_name = os.path.basename(urllib.url2pathname(url))
@@ -43,16 +56,16 @@ def get_img(url):
 
 # url = "http://www.69sexvideos.com"
 try:
-    user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
 
-    html_data = request_url(url)
+    headers = init_headers()
+    html_data = request_url(url,headers)
     soup = BeautifulSoup(html_data)
     get_content(1)
     
     pages = soup.select('#pagination a')
     num = 2
     for page in pages:
-        data = request_url(url+page['href']) 
+        data = request_url(url+page['href'],headers) 
         soup = BeautifulSoup(data)        
         get_content(num)
         num += 1
