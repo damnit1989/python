5b22c32ad0fdfff5dde489cf2b4ac504fe5cb888
Your Name <damnit1989@163.com>
2017-10-18 22:36:16 -0700

    简易爬取糗事百科段子，评论数，点赞数，图片

--- pachong1.py
+++ pachong1.py
@@ -1,57 +1,90 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
-# 爬虫实例1
+# 简易爬取糗事百科的段子,图片地址,点赞数,评论数  
+
+
+'''BeautifulSoup 爬虫实例1'''
 
 
 import urllib, urllib2
+from bs4 import BeautifulSoup
+import os
 #from lxml import etree
-page = 1
-url = "https://www.qiushibaike.com/hot/page/"+str(page)
 
-try:
-    user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+
+
+# 写入文件
+def write_to_file(content,filename):
+    with open(filename,'a') as f:
+        f.write(content.encode('utf-8'))
+
+        
+# 获取图片保存到本地 
+def get_img(url):
+    apath = os.path.abspath('.')
+    img_dir = apath+'/jsbk'
+    
+    # 判断目录是否存在,不存在则创建
+    if not os.path.isdir(img_dir):
+        os.mkdir(img_dir)
+    user_agent = 'User-Agent:Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
     headers = {'User-Agent':user_agent}
-    request = urllib2.Request(url,None,headers)
+    request = urllib2.Request('http:'+url,None,headers)
     u_handle = urllib2.urlopen(request)
-    ret = u_handle.read()
+    data = u_handle.read()
+    
+    # 截取url末尾的图片名称
+    img_name = os.path.basename(urllib.url2pathname(url))
+    with open(img_dir+'/'+img_name,'wb') as f:
+        f.write(data)
 
-    #html = etree.HTML(ret)
-    #nodes = html.xpath('//div[@id="content-left"]/div')
-    #for node in nodes:
-    #    print node.xpath('a/div/span/text()')
-    #print len(nodes)
-except urllib2.HTTPError, e:
-    print 'http请求 错误'
-    print e.reason
-except urllib2.URLError, e:
-    print 'url地址错误'
-    print e
-except Exception, e:
-    print e
-    print 'no '
-else:
-    print 'ok'
-    # print ret
+        
+# 简易爬取糗事百科的段子,图片地址,点赞数,评论数        
+def test():
+    page = 1
+    # url = "https://www.qiushibaike.com/text/page/"+str(page)
+    url = "https://www.qiushibaike.com/imgrank/page/"+str(page)
+    try:
+        # 构造请求头部
+        user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+        headers = {'User-Agent':user_agent}
+        request = urllib2.Request(url,None,headers)
+        
+        # 发送请求
+        u_handle = urllib2.urlopen(request)
+        
+        # 获取html内容
+        html_str = u_handle.read()
+        
+        #构造文档树
+        soup = BeautifulSoup(html_str)
+        
+        # 选择节点
+        divs = soup.select('div[id="content-left"] > div ')
+        for x in divs:
+            # data = {}
+            # data['内容'] = x.select('div[class="content"]')[0].get_text().strip()
+            # data['图片地址：'] = x.select('div[class="thumb"]')[0].a.img.get('src')
+            # data['好笑：'] = x.select('div[class="stats"]')[0].select('span[class="stats-vote"]')[0].get_text().strip()
+            # data['评论：'] = x.select('div[class="stats"]')[0].select('span[class="stats-comments"]')[0].a.get_text().strip()
+            # print data
+            
+            print '内容：',x.select('div[class="content"]')[0].get_text().strip()
+            print '图片地址：',x.select('div[class="thumb"]')[0].a.img.get('src')
+            print '好笑：',x.select('div[class="stats"]')[0].select('span[class="stats-vote"]')[0].get_text().strip()
+            print '评论：',x.select('div[class="stats"]')[0].select('span[class="stats-comments"]')[0].a.get_text().strip()
 
-
-
+            write_to_file(x.select('div[class="content"]')[0].get_text().strip()+'\n','qiushibaike.txt')
+            write_to_file(x.select('div[class="thumb"]')[0].a.img.get('src')+'\n\n','qiushibaike.txt')
+            get_img(x.select('div[class="thumb"]')[0].a.img.get('src'))
+            
+    except urllib2.HTTPError, e:
+        print e.reason        
+    except urllib2.URLError, e:
+        print e.reason
+    else:
+        print 'ok'
+        
 
 if __name__ == '__main__':
-    import urllib, urllib2
-    from bs4 import BeautifulSoup
-    page = 1
-    url = "https://www.qiushibaike.com/text/page/"+str(page)
-    user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-    headers = {'User-Agent':user_agent}
-    request = urllib2.Request(url,None,headers)
-    u_handle = urllib2.urlopen(request)
-    html_str = u_handle.read()
-    soup = BeautifulSoup(html_str)
-    divs = soup.select('div[id="content-left"] > div ')
-    for x in divs:
-        #print type(x)
-        #print x
-
-        print '内容：',x.select('div[class="content"]')[0].get_text().strip()
-        print '好笑：',x.select('div[class="stats"]')[0].select('span[class="stats-vote"]')[0].get_text()
-        print '评论：',x.select('div[class="stats"]')[0].select('span[class="stats-comments"]')[0].a.get_text()
+    test()
